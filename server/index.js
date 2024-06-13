7ec6669ca38a024c49283a8c4fb6a7074c195f45
Elfque <100863540+Elfque@users.noreply.github.com>
2024-06-13 23:18:05 +0100

    socket for messages

--- server/index.js
+++ server/index.js
@@ -20,35 +20,39 @@ app.use("/api/chats", chats);
 app.use("/api/message", message);
 
 const PORT = Number(process.env.PORT) || 4000;
-app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server started on Port ${PORT}`)
+);
 
-// const io = require("socket.io")(server, {
-//   pingTimeout: 60000,
-//   cors: { origin: "http://localhost:5173" },
-// });
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: { origin: process.env.CLIENT_URL },
+});
 
-// io.on("connection", (socket) => {
-//   socket.on("setup", (userData) => {
-//     socket.join(userData?._id);
-//     socket.emit("Connected");
-//   });
+io.on("connection", (socket) => {
+  socket.on("setup", (userData) => {
+    socket.join(userData?._id);
+    socket.emit("Connected");
+  });
 
-//   socket.on("join chat", (room) => {
-//     socket.join(room);
-//   });
+  socket.on("join chat", (room) => {
+    socket.join(room);
+  });
 
-//   socket.on("new message", (newMessageRecieved) => {
-//     var chat = newMessageRecieved.chat;
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
 
-//     if (!chat.users) return console.log("chat.users not defined");
+    if (!chat.users) return console.log("chat.users not defined");
 
-//     chat.users.forEach((user) => {
-//       if (user._id == newMessageRecieved.sender._id) return;
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender[0]._id) return;
 
-//       socket.in(user._id).emit("message recieved", newMessageRecieved);
-//     });
-//   });
-// });
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
 
-// const url =
-//   "mongodb+srv://faruqadeyemi70:WgFthf9gZWLN1TWQ@rays.b5masjk.mongodb.net/";
+  //   socket.off("setup", () => {
+  //     // console.log("USER DISCONNECTED");
+  //     socket.leave(userData._id);
+  //   });
+});
